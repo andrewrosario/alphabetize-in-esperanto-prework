4aa9c496d13cb301ad5f34ce58ac892e46bf4ad9
Andrew Rosario <rosario.andrew@gmail.com>
2019-07-26 01:50:24 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -4,9 +4,7 @@ def alphabetize(arr)
   alphabet = esperanto.split("")
   values = *(1..alphabet.length)
   arr.sort_by do |phrase|
-    phrase_value = 0
-    i = 0
-    while i < phrase.length
+    phrase_array = phrase.char
     values[alphabet.find_index(phrase[0])]
   end
 end
